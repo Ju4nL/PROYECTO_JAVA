13c2a658f67a657b6cd6d4ce73e8814868019e23
ezerutp <u22320821@utp.edu.pe>
2023-10-07 17:56:50 -0500

    CSVHandler

--- AppInventario/src/appinventario/storage/CSVHandler.java
+++ AppInventario/src/appinventario/storage/CSVHandler.java
@@ -1,6 +1,8 @@
 package appinventario.storage;
 
 import appinventario.models.Producto;
+import appinventario.models.Proveedor;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.io.*;
@@ -16,17 +18,27 @@ public class CSVHandler {
     }
 
     //Método para leer datos de Productos según la estructura del modelo
-    public List<Producto> leerProductos(String modelo) {
+    //ID_Producto,Nombre_Producto,Descripcion_Producto,Precio_Producto,CantidadStock_Producto,UnidadMedida_Producto,ID_Proveedor
+    public List<Producto> leerProductos() {
         List<Producto> productos = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String linea;
             while ((linea = br.readLine()) != null) {
                 String[] celdas = linea.split(",");
-                Producto producto = new Producto(Integer.parseInt(celdas[0]),celdas[1],celdas[2],Double.parseDouble(celdas[3]),Integer.parseInt(celdas[4]),celdas[5],);
+                int id = Integer.parseInt(celdas[0]);
+                String nombre = celdas[1];
+                String descripcion = celdas[2];
+                double precio = Double.parseDouble(celdas[3]);
+                int stock = Integer.parseInt(celdas[4]);
+                String unidadMedida = celdas[5];
+                //Solucionar esto 
+                Proveedor proveedor = new Proveedor();
+                //
+                Producto producto = new Producto(id,nombre,descripcion,precio,stock,unidadMedida,proveedor);
                 productos.add(producto);
             }
         } catch (IOException e) {
-
+            e.printStackTrace();
         }
         return productos;
     }

4afec2aed26c218752cab3d7610b067c9fb284d9
Juan Lozada <juanlozadacochaches@gmail.com>
2023-10-15 18:05:59 -0500

    update

--- AppInventario/src/appinventario/testezer.java
+++ AppInventario/src/appinventario/testezer.java
@@ -7,8 +7,13 @@ import appinventario.models.Usuario;
 public class testezer {
 
     public static void main(String[] arg) {
+<<<<<<< Updated upstream
         /*String pass = Seguridad.hashPassword("321");
         Usuario newuser = new Usuario( "Hillary", "Castañeda", "hillary1304", pass);
+=======
+        String pass = Seguridad.hashPassword("123");
+        Usuario newuser = new Usuario(1, "Ezer", "Vidarte", "ezerutp", pass);
+>>>>>>> Stashed changes
         CSV<Usuario> usercsv = new CSV<>(Usuario.class);
         if (usercsv.registrar(newuser) == true) {
             System.out.println("Registrado correctamente!");

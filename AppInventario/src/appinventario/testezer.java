25487d6319ef2453af9183b3c2532a1ed89d04b3
hillary1304 <rosaliayanime@gmail.com>
2023-10-15 17:57:38 -0500

    Prueba 2

--- AppInventario/src/appinventario/testezer.java
+++ AppInventario/src/appinventario/testezer.java
@@ -7,11 +7,11 @@ import appinventario.models.Usuario;
 public class testezer {
 
     public static void main(String[] arg) {
-        String pass = Seguridad.hashPassword("321");
+        /*String pass = Seguridad.hashPassword("321");
         Usuario newuser = new Usuario( "Hillary", "Castañeda", "hillary1304", pass);
         CSV<Usuario> usercsv = new CSV<>(Usuario.class);
         if (usercsv.registrar(newuser) == true) {
             System.out.println("Registrado correctamente!");
-        }
+        }*/
     }
 }

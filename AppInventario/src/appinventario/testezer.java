cc29098ebe203f852fe41fbe3fb69e2cf296784b
hillary1304 <rosaliayanime@gmail.com>
2023-10-15 17:50:04 -0500

    Prueba en vs code

--- AppInventario/src/appinventario/testezer.java
+++ AppInventario/src/appinventario/testezer.java
@@ -7,8 +7,8 @@ import appinventario.models.Usuario;
 public class testezer {
 
     public static void main(String[] arg) {
-        String pass = Seguridad.hashPassword("123");
-        Usuario newuser = new Usuario( "Ezer", "Vidarte", "ezerutp", pass);
+        String pass = Seguridad.hashPassword("321");
+        Usuario newuser = new Usuario( "Hillary", "Castañeda", "hillary1304", pass);
         CSV<Usuario> usercsv = new CSV<>(Usuario.class);
         if (usercsv.registrar(newuser) == true) {
             System.out.println("Registrado correctamente!");
